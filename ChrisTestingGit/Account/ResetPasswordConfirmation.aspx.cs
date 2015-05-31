72e6fb4d4926100ebe3cae58100afe28e5134252
Chris Biles <chris@crazykentucky.com>
2015-05-31 19:37:25 -0400

    This is a test for commiting files to github.  Interesting...

---
+++ ChrisTestingGit/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ChrisTestingGit.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ ChrisTestingGit/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChrisTestingGit.Startup))]
+namespace ChrisTestingGit
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
